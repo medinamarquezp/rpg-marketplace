0f052f8a228448eeec426f1145977bd11cebb642
Pedro Medina Márquez <medinamarquezp@gmail.com>
2023-03-21 20:47:04 +0100

    chore: marketplace initial deployment

---
+++ migrations/01_rpg_marketplace_deployment.js
@@ -0,0 +1,8 @@
+const RPGERC1155 = artifacts.require("RPGERC1155");
+const RPGMarketplace = artifacts.require("RPGMarketplace");
+
+module.exports = async function (deployer) {
+  await deployer.deploy(RPGERC1155);
+  const rpgERC1155 = await RPGERC1155.deployed();
+  await deployer.deploy(RPGMarketplace, rpgERC1155.address);
+};
