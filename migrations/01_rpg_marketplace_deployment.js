1b188d3a5ca02358f68a531d45f10b2028efc1ba
Pedro Medina Márquez <medinamarquezp@gmail.com>
2023-03-22 14:08:19 +0100

    chore: authorize marketplace address on ERC1155 contract

--- migrations/01_rpg_marketplace_deployment.js
+++ migrations/01_rpg_marketplace_deployment.js
@@ -5,4 +5,6 @@ module.exports = async function (deployer) {
   await deployer.deploy(RPGERC1155);
   const rpgERC1155 = await RPGERC1155.deployed();
   await deployer.deploy(RPGMarketplace, rpgERC1155.address);
+  const marketplace = await RPGMarketplace.deployed();
+  await rpgERC1155.authorize(marketplace.address, true);
 };
